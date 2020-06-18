c96090b1b44e8a10c8e019273a596ee9e47a57aa
richard <richard.ngo94@myhunter.cuny.edu>
2020-06-18 18:52:20 -0400

    pop up button opens new window on click    its added in the main file and has on click functions
     -got to work on the close function of the thing or can have close on click outside later
    
     -new window with fields retaining to the design
    
     -change background color to grey when on click

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.Set;
 
@@ -13,8 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
     //Intial Variable List
     int taskAmount = 0;
+    Button addTaskButton;
 
     public class Task {
+
         boolean completed = false;
         boolean recurring = false;
         int position;
@@ -35,7 +43,23 @@ public class MainActivity extends AppCompatActivity {
         //Creating new preferences and the editor
         SharedPreferences taskList = getSharedPreferences("TASK_LIST", Context.MODE_PRIVATE);
         final SharedPreferences.Editor editor = taskList.edit();
+        LinearLayout apple = (LinearLayout ) findViewById(R.id.thebigthinglayout);
+        apple.setBackgroundColor(Color.WHITE);
         editor.apply();
+        addTaskButton =(Button)findViewById(R.id.addTaskButton);
+
+
+    }
+    public void addTaskPopUp(View v){
+        if(v == addTaskButton){
+            startActivity(new Intent(MainActivity.this,Pop.class));
+
+            LinearLayout owo = (LinearLayout ) findViewById(R.id.thebigthinglayout);
+            owo.setBackgroundColor(Color.GRAY);
+
+
+        }
+
     }
 
 }
